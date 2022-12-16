9aa4d09d64a3e6e8ebdd07f93a3a9c2e27c01e8a
Oleg Semenov <66306538+semoleg1986@users.noreply.github.com>
2022-12-16 22:19:43 +0300

    Create insertionSort.py

---
+++ insertionSort.py
@@ -0,0 +1,18 @@
+import time
+arr=[]
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j>0 and arr[j-1]>arr[j]:
+            arr[j],arr[j-1]=arr[j-1], arr[j]
+            j-=1
+if __name__ == "__main__":
+    start = time.time()
+    for i in range(8):
+        x = int(input(''))
+        arr.append(x)
+    # arr = [5,4,1,8,7,2,6,3]
+    insertionSort(arr)
+    print(arr)
+    end = time.time() - start
+    print('время: ',end)
